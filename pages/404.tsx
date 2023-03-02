099a2f520ae6c5f92cce2480c6ac692f56d34ca2
Sebastian <rockmadero@gmail.com>
2023-03-02 14:30:03 -0500

    404

---
+++ pages/404.tsx
@@ -0,0 +1,17 @@
+import { ShopLayout } from '@/components/layouts'
+import { Typography } from '@mui/material'
+import { Box } from '@mui/system'
+import React from 'react'
+
+const Page404 = () => {
+  return (
+    <ShopLayout title='Page not found' pageDesc="Didn't find it">
+          <Box display='flex' justifyContent='center' alignItems='center' height='calc(100vh - 200px)' sx={{ flexDirection: { xs: 'column', sm: 'row' }}}>
+            <Typography variant='h1' component='h1' fontSize={80} fontWeight={200}> 404 |</Typography>
+            <Typography marginLeft={2}>No se puedo encontrar la pagina :(</Typography>
+        </Box>
+    </ShopLayout>
+  )
+}
+
+export default Page404;
