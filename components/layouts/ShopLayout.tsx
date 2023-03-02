a7528fbf704b856ca7867a9e3db253104265e6b3
Sebastian <rockmadero@gmail.com>
2023-03-02 12:36:40 -0500

    layout shop

---
+++ components/layouts/ShopLayout.tsx
@@ -0,0 +1,45 @@
+import Head from "next/head"
+import { FC, ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+  title: string;
+  pageDesc: string;
+  imageUrl?: string;
+}
+
+export const ShopLayout: FC<Props> = ({ children, title, pageDesc, imageUrl }) => {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="og:name" content={title} />
+        <meta name="description" content={pageDesc} />
+        <meta name="og:description" content={pageDesc} />
+        {
+          imageUrl &&(
+            <meta name="og:image" content={ imageUrl } />
+          )
+        }
+      </Head>
+
+      <nav>
+        {/* Navbar */}
+      </nav>
+
+      {/* Sidebar */}
+
+      <main style={{
+        margin: '80px auto',
+        maxWidth: '1440px',
+        padding: '0 30px'
+      }}>
+        {children}
+      </main>
+
+      <footer>
+        {/* Todo: */}
+      </footer>
+    </>
+  )
+}
