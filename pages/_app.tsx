e6d286dc75b92f7c562195cf413772550d7eb7dd
Sebastian <rockmadero@gmail.com>
2023-03-02 13:57:29 -0500

    dark theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,11 @@
 import type { AppProps } from 'next/app'
 import { CssBaseline, ThemeProvider } from '@mui/material'
-import { lightTheme } from '../themes';
+import { lightTheme, darkTheme } from '../themes';
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={lightTheme}>
+    <ThemeProvider theme={darkTheme}>
       <CssBaseline/>
       <Component {...pageProps} />
     </ThemeProvider>
