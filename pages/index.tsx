a4f951c15ac76bfc26ed5957774cd1b6c9165a03
Sebastian <rockmadero@gmail.com>
2023-03-17 15:09:30 -0500

    useing swr globally

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,23 @@
-import { Typography } from '@mui/material';
-import { ShopLayout } from '../components/layouts';
-import { initialData } from '@/data/products';
-import { ProductList } from '@/components/products';
-
+import { Typography } from "@mui/material";
+import { ShopLayout } from "../components/layouts";
+import { initialData } from "@/data/products";
+import { ProductList } from "@/components/products";
+import { useProducts } from "@/hooks";
 
 export default function Home() {
+  const { products, isError, isLoading } = useProducts("/products");
   return (
-    <ShopLayout title='Teslo Shop | Home' pageDesc='Ecommerce clon de teslo shop'>
-      <Typography variant='h1' component='h1'>Home</Typography>
-      <Typography variant='h2' component='h2' sx={{ mb: 1 }}>Todos los productos</Typography>
-
-      <ProductList 
-        products={initialData.products as any}
-      />
+    <ShopLayout
+      title="Teslo Shop | Home"
+      pageDesc="Ecommerce clon de teslo shop"
+    >
+      <Typography variant="h1" component="h1">
+        Home
+      </Typography>
+      <Typography variant="h2" component="h2" sx={{ mb: 1 }}>
+        Todos los productos
+      </Typography>
+      {isLoading ? <h1>Cargando...</h1> : <ProductList products={products} />}
     </ShopLayout>
-  )
+  );
 }
