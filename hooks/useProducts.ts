2ae2b0349254972b7d235ec3ee8fc0cfbbc146fa
Sebastian <rockmadero@gmail.com>
2023-03-17 15:09:11 -0500

    useProducts hook

---
+++ hooks/useProducts.ts
@@ -0,0 +1,17 @@
+import { IProduct } from "@/interfaces";
+import useSWR, { SWRConfiguration } from "swr";
+
+export const useProducts = (url: string, config: SWRConfiguration = {}) => {
+  //   const { data, error, isLoading } = useSWR<IProduct[]>(
+  //     `/api${url}`,
+  //     fetcher,
+  //     config
+  //   );
+  const { data, error, isLoading } = useSWR<IProduct[]>(`/api${url}`, config);
+
+  return {
+    products: data,
+    isLoading,
+    isError: error,
+  };
+};
