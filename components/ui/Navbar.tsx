fbfb8de5e602f3300e22c0ff604213adf167ddda
Sebastian <rockmadero@gmail.com>
2023-03-19 11:07:43 -0500

    navabar path active

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,61 +1,86 @@
+import { useRouter } from "next/router";
 import NextLink from "next/link";
-import { AppBar, Badge, Button, IconButton, Link, Toolbar, Typography } from "@mui/material"
+import {
+  AppBar,
+  Badge,
+  Button,
+  IconButton,
+  Link,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import { Box } from "@mui/system";
 import { SearchOutlined, ShoppingCartOutlined } from "@mui/icons-material";
 
 export const Navbar = () => {
-    return (
-        <AppBar>
-            <Toolbar>
-                <NextLink href="/" passHref>
-                    <Link component={'span'} display='flex' alignItems='center'>
-                        <Typography variant="h6" component="h6">Next |</Typography>
-                        <Typography component="p" sx={{ ml: 0.5 }}>Shop</Typography>
-                    </Link>
-                </NextLink>
+  const { pathname } = useRouter();
 
-                <Box flex={1} />
+  return (
+    <AppBar>
+      <Toolbar>
+        <NextLink href="/" passHref>
+          <Link component={"span"} display="flex" alignItems="center">
+            <Typography variant="h6" component="h6">
+              Next |
+            </Typography>
+            <Typography component="p" sx={{ ml: 0.5 }}>
+              Shop
+            </Typography>
+          </Link>
+        </NextLink>
 
-                <Box sx={{ display: { xs: 'none', sm:'block' } }}>
-                    <NextLink href='/category/men' passHref>
-                        <Link component={'span'}>
-                            <Button>Hombres</Button>
-                        </Link>
-                    </NextLink>
-                   
-                    <NextLink href='/category/women' passHref>
-                        <Link component={'span'}>
-                            <Button>Mujeres</Button>
-                        </Link>
-                    </NextLink>
-                    
-                    <NextLink href='/category/kids' passHref>
-                        <Link component={'span'}>
-                            <Button>Niños</Button>
-                        </Link>
-                    </NextLink>
-                </Box>
+        <Box flex={1} />
 
-                <Box flex={1} />
+        <Box sx={{ display: { xs: "none", sm: "block" } }}>
+          <NextLink href="/category/men" passHref>
+            <Link component={"span"}>
+              <Button color={pathname === "/category/men" ? "primary" : "info"}>
+                Hombres
+              </Button>
+            </Link>
+          </NextLink>
 
-                <IconButton>
-                    <SearchOutlined />
-                </IconButton>
+          <NextLink href="/category/women" passHref>
+            <Link component={"span"}>
+              <Button
+                color={
+                  pathname.includes("/category/women") ? "primary" : "info"
+                }
+              >
+                Mujeres
+              </Button>
+            </Link>
+          </NextLink>
 
-                <NextLink href='/cart' passHref>
-                    <Link component={'span'}>
-                        <IconButton>
-                            <Badge badgeContent={2} color="secondary">
-                                <ShoppingCartOutlined />
-                            </Badge>
-                        </IconButton>
-                    </Link>
-                </NextLink> 
+          <NextLink href="/category/kids" passHref>
+            <Link component={"span"}>
+              <Button
+                color={pathname.includes("/category/kid") ? "primary" : "info"}
+              >
+                Niños
+              </Button>
+            </Link>
+          </NextLink>
+        </Box>
 
-                <Button>
-                    Menú
-                </Button>
-            </Toolbar>
-        </AppBar>
-    )
-}
+        <Box flex={1} />
+
+        <IconButton>
+          <SearchOutlined />
+        </IconButton>
+
+        <NextLink href="/cart" passHref>
+          <Link component={"span"}>
+            <IconButton>
+              <Badge badgeContent={2} color="secondary">
+                <ShoppingCartOutlined />
+              </Badge>
+            </IconButton>
+          </Link>
+        </NextLink>
+
+        <Button>Menú</Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
