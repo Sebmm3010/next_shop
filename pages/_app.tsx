c0215955c81e071e0ed40c0cb8af895117b9cb00
Sebastian <rockmadero@gmail.com>
2023-03-19 11:23:36 -0500

    uiProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app";
 import { SWRConfig } from "swr";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import { UiProvider } from "../context";
 import { lightTheme, darkTheme } from "../themes";
 import "@/styles/globals.css";
 
@@ -12,10 +13,12 @@ export default function App({ Component, pageProps }: AppProps) {
           fetch(resource, init).then((res) => res.json()),
       }}
     >
-      <ThemeProvider theme={darkTheme}>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <UiProvider>
+        <ThemeProvider theme={darkTheme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </UiProvider>
     </SWRConfig>
   );
 }
