f80d2be58672f59e506bd4dc1617d8e9346ba731
Sebastian <rockmadero@gmail.com>
2023-03-21 11:34:54 -0500

    search page working

--- pages/search/[query].tsx
+++ pages/search/[query].tsx
@@ -1,24 +1,51 @@
 import { Typography } from "@mui/material";
 import { ShopLayout } from "../../components/layouts";
 import { ProductList } from "@/components/products";
-import { useProducts } from "@/hooks";
-import { FullScreenLoading } from "../../components/ui";
 
-const SearchPage=()=> {
-  const { products, isLoading } = useProducts("/search/pac");
+
+interface Props{
+    products: IProduct[]
+}
+
+const SearchPage:NextPage<Props> = ({products}) => {
   return (
-    <ShopLayout title="Next Shop | Buscar" pageDesc="Pagina de busquedas de Nextshop">
+    <ShopLayout
+      title="Next Shop | Buscar"
+      pageDesc="Pagina de busquedas de Nextshop"
+    >
       <Typography variant="h1" component="h1">
         Buscar producto
       </Typography>
       <Typography variant="h2" component="h2" sx={{ mb: 1 }}>
         ABC----12213
       </Typography>
-      {isLoading ? <FullScreenLoading /> : <ProductList products={products} />}
+      <ProductList products={products} />
     </ShopLayout>
   );
-}
+};
 
+import { GetServerSideProps, NextPage } from "next";
+import { dbProducts } from "@/data";
+import { IProduct } from '../../interfaces/products';
 
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const { query = "" } = params as { query: string };
+  if (query.length === 0) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: true,
+      },
+    };
+  }
+
+  let products = await dbProducts.getProductsByTerms(query);
+
+  return {
+    props: {
+        products
+    },
+  };
+};
 
 export default SearchPage;
