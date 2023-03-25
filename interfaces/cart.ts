0f9ed9dbf5c9f1092a930b236110430e115b3bbd
Sebastian <rockmadero@gmail.com>
2023-03-25 13:27:27 -0500

    cart products interface

---
+++ interfaces/cart.ts
@@ -0,0 +1,14 @@
+import { ISize } from "./";
+
+export interface ICartProduct {
+  _id: string;
+  description: string;
+  image: string;
+  inStock: number;
+  price: number;
+  size: ISize;
+  slug: string;
+  title: string;
+  gender: "hombre" | "mujer" | "infantil" | "unisex";
+  quantity: number;
+}
