b27cf4dde395f8dd7d9a7e21737e142fd7ab1837
Sebastian <rockmadero@gmail.com>
2023-03-25 13:27:44 -0500

    applying cart context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from "next/app";
 import { SWRConfig } from "swr";
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import { UiProvider } from "../context";
+import { CartProvider, UiProvider } from "../context";
 import { lightTheme, darkTheme } from "../themes";
 import "@/styles/globals.css";
 
@@ -13,12 +13,14 @@ export default function App({ Component, pageProps }: AppProps) {
           fetch(resource, init).then((res) => res.json()),
       }}
     >
-      <UiProvider>
-        <ThemeProvider theme={darkTheme}>
-          <CssBaseline />
-          <Component {...pageProps} />
-        </ThemeProvider>
-      </UiProvider>
+      <CartProvider>
+        <UiProvider>
+          <ThemeProvider theme={darkTheme}>
+            <CssBaseline />
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </UiProvider>
+      </CartProvider>
     </SWRConfig>
   );
 }
