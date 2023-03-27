4cc92ee9534e427ed5b2c059c90b71f93b408892
Sebastian madero <rockmadero@gmail.com>
2023-03-27 16:18:41 -0500

    seed modified for users

--- pages/api/seed.ts
+++ pages/api/seed.ts
@@ -1,19 +1,26 @@
-import { db, seedData } from '@/data';
-import { Product } from '@/models';
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { db, seedData } from "@/data";
+import { Product, User } from "@/models";
+import type { NextApiRequest, NextApiResponse } from "next";
 
-type Data = { msg: string }
+type Data = { msg: string };
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  if (process.env.NODE_ENV === "production") {
+    return res.status(401).json({ msg: "No tiene acceso a este API" });
+  }
 
-    if(process.env.NODE_ENV==='production'){
-        return res.status(401).json({msg:'No tiene acceso a este API'})
-    }
+  await db.connect();
 
-    await db.connect();
-    await Product.deleteMany();
-    await Product.insertMany(seedData.initialData.products);
-    await db.disconnect();
+  await User.deleteMany();
+  await Product.deleteMany();
 
-    res.status(200).json({ msg: 'Proceso realizado correcatamente' });
+  await Product.insertMany(seedData.initialData.products);
+  await User.insertMany(seedData.initialData.users);
+  
+  await db.disconnect();
+
+  res.status(200).json({ msg: "Proceso realizado correcatamente" });
 }
