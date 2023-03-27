e3a52af33e537409e43d0b5ac8dd7633a223bd8f
Sebastian madero <rockmadero@gmail.com>
2023-03-27 16:43:40 -0500

    login endpoit

---
+++ pages/api/user/login.ts
@@ -0,0 +1,58 @@
+import { db } from "@/data";
+import { User } from "@/models";
+import type { NextApiRequest, NextApiResponse } from "next";
+import bcrypt from "bcrypt";
+
+type Data =
+  | { msg: string }
+  | {
+      token: string;
+      user: {
+        name: string;
+        email: any;
+        role: string;
+      };
+    };
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  switch (req.method) {
+    case "POST":
+      return loginUser(req, res);
+
+    default:
+      return res.status(400).json({ msg: "Bad request" });
+  }
+}
+
+const loginUser = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const { email = "", password = "" } = req.body;
+
+  await db.connect();
+  const user = await User.findOne({ email });
+  await db.disconnect();
+
+  if (!user) {
+    return res
+      .status(400)
+      .json({ msg: "Correo o contraseña invalidos - EMAIL" });
+  }
+  if (!bcrypt.compareSync(password, user.password!)) {
+    return res
+      .status(400)
+      .json({ msg: "Correo o contraseña no validos - contraseña" });
+  }
+
+  const { role, name } = user;
+
+  return res.status(200).json({
+    token: "", //JWT
+    user: {
+      name,
+      email,
+      role,
+    },
+  });
+};
