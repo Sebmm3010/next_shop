9f0d5df414e63961f2a6e86cc09cd217609acf85
Sebastian madero <rockmadero@gmail.com>
2023-03-30 16:07:34 -0500

    using react hook form for inputs

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import NextLink from "next/link";
-import { AuthLayout } from "@/components/layouts";
 import {
   Box,
   Grid,
@@ -12,63 +11,108 @@ import {
   Link,
 } from "@mui/material";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
+import { useForm } from "react-hook-form";
+import { AuthLayout } from "@/components/layouts";
+import { validation } from "@/utils";
+
+interface FormData {
+  email: string;
+  password: string;
+}
 
 const LoginPage = () => {
   const [showPassword, setShowPassword] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  const handleLogin = (data: FormData) => {
+    console.log({ data });
+  };
+
   return (
     <AuthLayout title="Ingresar">
-      <Box sx={{ width: 350, padding: "10px 20px" }}>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <Typography variant="h1" component="h1">
-              Iniciar sesión
-            </Typography>
-          </Grid>
+      <form onSubmit={handleSubmit(handleLogin)} noValidate>
+        <Box sx={{ width: 350, padding: "10px 20px" }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Typography variant="h1" component="h1" textAlign="center">
+                Iniciar sesión
+              </Typography>
+            </Grid>
 
-          <Grid item xs={12}>
-            <TextField label="Correo" variant="filled" fullWidth />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              label="Contraseña"
-              type={showPassword ? "password" : "text"}
-              variant="filled"
-              fullWidth
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={() => setShowPassword(!showPassword)}
-                      onMouseDown={() => setShowPassword(!showPassword)}
-                    >
-                      {showPassword ? <Visibility /> : <VisibilityOff />}
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <Button
-              color="secondary"
-              className="circular-btn"
-              size="large"
-              fullWidth
-            >
-              Ingresar
-            </Button>
-          </Grid>
+            <Grid item xs={12}>
+              <TextField
+                label="Correo"
+                variant="filled"
+                fullWidth
+                {...register("email", {
+                  required: "El email es obligatorio",
+                  validate: validation.isEmail,
+                })}
+                error={!!errors.email}
+                helperText={errors.email?.message}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                label="Contraseña"
+                type={!showPassword ? "password" : "text"}
+                variant="filled"
+                fullWidth
+                {...register("password", {
+                  required: "La contraseña es obligatoria",
+                  minLength: {
+                    value: 6,
+                    message: "La contraseña debe tener minimo 6 caracteres",
+                  },
+                })}
+                error={!!errors.password}
+                helperText={errors.password?.message}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={() => setShowPassword(!showPassword)}
+                        onMouseDown={() => setShowPassword(!showPassword)}
+                      >
+                        {showPassword ? <Visibility /> : <VisibilityOff />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                type="submit"
+                color="secondary"
+                className="circular-btn"
+                size="large"
+                fullWidth
+              >
+                Ingresar
+              </Button>
+            </Grid>
 
-          <Grid item xs={12} display="flex" justifyContent="end">
-            <NextLink href="/auth/registro" passHref>
-              <Link underline="always" component="span">
-                No tienes cuenta?
-              </Link>
-            </NextLink>
+            <Grid item xs={12} display="flex" justifyContent="end">
+              <NextLink href="/auth/registro" passHref>
+                <Link
+                  underline="always"
+                  component="span"
+                  sx={{ ":hover": { color: "#0535f5" } }}
+                >
+                  No tienes cuenta?
+                </Link>
+              </NextLink>
+            </Grid>
           </Grid>
-        </Grid>
-      </Box>
+        </Box>
+      </form>
     </AuthLayout>
   );
 };
