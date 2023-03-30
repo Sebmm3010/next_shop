637500b9b6aeea5ce16b872675e189fb68d9614a
Sebastian madero <rockmadero@gmail.com>
2023-03-30 18:16:56 -0500

    register page working

--- pages/auth/registro.tsx
+++ pages/auth/registro.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import NextLink from "next/link";
-import { AuthLayout } from "@/components/layouts";
+import { useForm } from "react-hook-form";
 import {
   Box,
   Grid,
@@ -10,91 +10,187 @@ import {
   InputAdornment,
   IconButton,
   Link,
+  Chip,
 } from "@mui/material";
-import { Visibility, VisibilityOff } from "@mui/icons-material";
+import { Visibility, VisibilityOff, ErrorOutline } from '@mui/icons-material';
+import { AuthLayout } from "@/components/layouts";
+import { validation } from "@/utils";
+import { nextShopApi } from "@/api";
+
+interface FormData {
+  name: string;
+  email: string;
+  password: string;
+  password2: string;
+}
 
 const RegisterPage = () => {
   const [showPassword, setShowPassword] = useState(false);
+  const [showError, setShowError] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm<FormData>();
+  const handleRegister = async({name, email, password}: FormData) => {
+    try {
+      const {data}= await nextShopApi.post("/user/register", {name, email, password})
+      const { token, user } = data;
+      console.log({token, user});
+    } catch (error) {
+      console.log("Usuario ya registrado");
+      setShowError(true);
+      setTimeout(() => {
+        setShowError(false);
+      }, 3000);
+    }
+  };
   return (
     <AuthLayout title="Registrarse">
-      <Box sx={{ width: 350, padding: "10px 20px" }}>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <Typography variant="h1" component="h1">
-              Crear cuenta
-            </Typography>
-          </Grid>
-
-          <Grid item xs={12}>
-            <TextField type="email" label="Correo" variant="filled" fullWidth />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              label="Contraseña"
-              type={!showPassword ? "password" : "text"}
-              variant="filled"
-              fullWidth
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={() => setShowPassword(!showPassword)}
-                      onMouseDown={() => setShowPassword(!showPassword)}
-                    >
-                      {showPassword ? <Visibility /> : <VisibilityOff />}
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              label="Confirmar contraseña"
-              type={!showPassword ? "password" : "text"}
-              variant="filled"
-              fullWidth
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={() => setShowPassword(!showPassword)}
-                      onMouseDown={() => setShowPassword(!showPassword)}
-                    >
-                      {showPassword ? <Visibility /> : <VisibilityOff />}
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </Grid>
-
-          <Grid item xs={12}>
-            <Button
-              color="secondary"
-              className="circular-btn"
-              size="large"
-              fullWidth
+      <form onSubmit={handleSubmit(handleRegister)} noValidate>
+        <Box sx={{ width: 350, padding: "10px 20px" }}>
+          <Grid container spacing={2}>
+            <Grid
+              item
+              xs={12}
+              display="flex"
+              flexDirection="column"
+              justifyContent="center"
             >
-              Registrarse
-            </Button>
-          </Grid>
+              <Typography variant="h1" component="h1" textAlign="center">
+                Crear cuenta
+              </Typography>
+              {showError && (
+                <Chip
+                  className="fadeIn"
+                  label="Correo ya en uso"
+                  color="error"
+                  icon={<ErrorOutline />}
+                />
+              )}
+            </Grid>
 
-          <Grid item xs={12} display="flex" justifyContent="end">
-            <NextLink href="/auth/login" passHref>
-              <Link
-                underline="always"
-                component="span"
-                sx={{ ":hover": { color: "#0535f5" } }}
+            <Grid item xs={12}>
+              <TextField
+                type="text"
+                label="Nombre"
+                variant="filled"
+                fullWidth
+                {...register("name", {
+                  required: "El nombre es obligatorio",
+                  minLength: {
+                    value: 2,
+                    message: "Nombre de tener minimo 2 caracteres",
+                  },
+                })}
+                error={!!errors.name}
+                helperText={errors.name?.message}
+              />
+            </Grid>
+
+            <Grid item xs={12}>
+              <TextField
+                type="email"
+                label="Correo"
+                variant="filled"
+                fullWidth
+                {...register("email", {
+                  required: "El email es obligatorio",
+                  validate: validation.isEmail,
+                })}
+                error={!!errors.email}
+                helperText={errors.email?.message}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                label="Contraseña"
+                type={!showPassword ? "password" : "text"}
+                variant="filled"
+                fullWidth
+                {...register("password", {
+                  required: "La contraseña es obligatoria",
+                  minLength: {
+                    value: 6,
+                    message: "La contraseña debe tener minimo 6 caracteres",
+                  },
+                })}
+                error={!!errors.password}
+                helperText={errors.password?.message}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={() => setShowPassword(!showPassword)}
+                        onMouseDown={() => setShowPassword(!showPassword)}
+                      >
+                        {!showPassword ? <Visibility /> : <VisibilityOff />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                label="Confirmar contraseña"
+                type={!showPassword ? "password" : "text"}
+                variant="filled"
+                fullWidth
+                {...register("password2", {
+                  required: "Las contraseñas no coinciden",
+                  validate: (values, formValues) =>
+                    values === formValues.password
+                      ? undefined
+                      : "Las contraseñas no coinciden",
+                })}
+                error={!!errors.password2}
+                helperText={errors.password2?.message}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={() => setShowPassword(!showPassword)}
+                        onMouseDown={() => setShowPassword(!showPassword)}
+                      >
+                        {!showPassword ? <Visibility /> : <VisibilityOff />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </Grid>
+
+            <Grid item xs={12}>
+              <Button
+                color="secondary"
+                className="circular-btn"
+                size="large"
+                fullWidth
+                type="submit"
               >
-                Ya tienes cuenta?
-              </Link>
-            </NextLink>
+                Registrarse
+              </Button>
+            </Grid>
+
+            <Grid item xs={12} display="flex" justifyContent="end">
+              <NextLink href="/auth/login" passHref>
+                <Link
+                  underline="always"
+                  component="span"
+                  sx={{ ":hover": { color: "#0535f5" } }}
+                >
+                  Ya tienes cuenta?
+                </Link>
+              </NextLink>
+            </Grid>
           </Grid>
-        </Grid>
-      </Box>
+        </Box>
+      </form>
     </AuthLayout>
   );
 };
