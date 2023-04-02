160de6fd2b845c4f317887e375d1b49699d65aef
Sebastian madero <rockmadero@gmail.com>
2023-04-02 12:06:36 -0500

    removing address cookies on logout

--- context/auth/AuthProvider.tsx
+++ context/auth/AuthProvider.tsx
@@ -100,6 +100,14 @@ export const AuthProvider: FC<Props> = ({ children }) => {
   const logoutUser = () => {
     Cookies.remove("token");
     Cookies.remove("cart");
+    Cookies.remove("name");
+    Cookies.remove("lastName");
+    Cookies.remove("address1");
+    Cookies.remove("address2");
+    Cookies.remove("city");
+    Cookies.remove("postalCode");
+    Cookies.remove("country");
+    Cookies.remove("phone");
     router.reload();
     dispatch({ type: "[Auth] - Logout" });
   };
