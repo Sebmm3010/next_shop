839414e72f879c533c4f27e0a4ba0f573c5b2643
Sebastian madero <rockmadero@gmail.com>
2023-04-04 13:17:34 -0500

    order model

--- models/Order.ts
+++ models/Order.ts
@@ -3,13 +3,41 @@ import { IOrder } from "@/interfaces";
 
 const orderSchema = new Schema(
   {
-    name: { type: String, require: true },
-    
+    // name: {},
+    user: { type: Schema.Types.ObjectId, ref: "User", require: true },
+    orderItem: [
+      {
+        _id: { type: Schema.Types.ObjectId, ref: "Product", required: true },
+        title: { type: String, required: true },
+        size: { type: String, required: true },
+        quantity: { type: Number, required: true },
+        slug: { type: String, required: true },
+        image: { type: String, required: true },
+        price: { type: Number, required: true },
+      },
+    ],
+    shippingAddress: {
+      name: { type: String, required: true },
+      lastName: { type: String, required: true },
+      address1: { type: String, required: true },
+      address2: { type: String },
+      city: { type: String, required: true },
+      postalCode: { type: String, required: true },
+      country: { type: String, required: true },
+      phone: { type: String, required: true },
+    },
+    numberOfItems: { type: Number, required: true },
+    subTotal: { type: Number, required: true },
+    iva: { type: Number, required: true },
+    total: { type: Number, required: true },
+    isPaid: { type: Boolean, required: true, default: false },
+    paidAt: { type: String },
   },
   {
     timestamps: true,
   }
 );
 
-const Order: Model<IOrder> = mongoose.models.Order || model("Order", orderSchema);
+const Order: Model<IOrder> =
+  mongoose.models.Order || model("Order", orderSchema);
 export default Order;
