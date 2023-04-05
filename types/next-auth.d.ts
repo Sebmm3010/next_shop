4e6289cacab492e1c846313375a8b0f2467f2cba
Sebastian madero <rockmadero@gmail.com>
2023-04-05 16:49:34 -0500

    Session types for users

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -4,4 +4,13 @@ declare module "next-auth" {
   interface DefaultSession {
     accessToken?: string;
   }
+  interface Session {
+    user: {
+      _id: string;
+      name: string;
+      email: string;
+      role: string;
+      id?: string;
+    };
+  }
 }
