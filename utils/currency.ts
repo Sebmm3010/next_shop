5b1a0d7d6be79dd9927737251d86c93e62d1e795
Sebastian madero <rockmadero@gmail.com>
2023-04-06 14:38:47 -0500

    from pesos to dollar

--- utils/currency.ts
+++ utils/currency.ts
@@ -1,12 +1,16 @@
-export const format= (value:number)=>{
+export const format = (value: number) => {
+  // Formateador
+  const formatter = new Intl.NumberFormat("es-CO", {
+    style: "currency",
+    currency: "COP",
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  });
 
-    // Formateador
-    const formatter= new Intl.NumberFormat('es-CO',{
-        style:"currency",
-        currency:"COP",
-        minimumFractionDigits:0,
-        maximumFractionDigits:0
-    });
+  return formatter.format(value);
+};
 
-    return formatter.format(value);
-}
+export const pesosToDollar = (pesos: number): number => {
+  const dollar = pesos / Number(process.env.NEXT_DOLAR_EXCHANGE_RATE);
+  return Math.round(dollar * 100) / 100;
+};
