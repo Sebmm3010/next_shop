18745731dc97e7fddc62f4e078df88d0f30b4de0
Sebastian madero <rockmadero@gmail.com>
2023-04-08 11:40:12 -0500

    using admin summary

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,5 +1,62 @@
+import { Grid } from "@mui/material";
+import {
+  AccessTimeOutlined,
+  AttachMoneyOutlined,
+  CancelPresentationOutlined,
+  CategoryOutlined,
+  CreditCardOffOutlined,
+  DashboardOutlined,
+  GroupOutlined,
+  ProductionQuantityLimitsOutlined,
+} from "@mui/icons-material";
+
+import { AdminSummary, PropsSummary } from "@/components/admin";
 import { AdminLayout } from "@/components/layouts";
-import { DashboardOutlined } from "@mui/icons-material";
+
+const summaryTile: PropsSummary[] = [
+  {
+    title: 1,
+    subTitle: "Ordenes totales",
+    icon: <CreditCardOffOutlined color="secondary" sx={{ fontSize: 40 }} />,
+  },
+  {
+    title: 2,
+    subTitle: "Ordenes pagadas",
+    icon: <AttachMoneyOutlined color="success" sx={{ fontSize: 40 }} />,
+  },
+  {
+    title: 3,
+    subTitle: "Ordenes pendientes",
+    icon: <CreditCardOffOutlined color="error" sx={{ fontSize: 40 }} />,
+  },
+  {
+    title: 4,
+    subTitle: "Clientes",
+    icon: <GroupOutlined color="primary" sx={{ fontSize: 40 }} />,
+  },
+  {
+    title: 5,
+    subTitle: "Productos",
+    icon: <CategoryOutlined color="warning" sx={{ fontSize: 40 }} />,
+  },
+  {
+    title: 6,
+    subTitle: "Sin existencia",
+    icon: <CancelPresentationOutlined color="error" sx={{ fontSize: 40 }} />,
+  },
+  {
+    title: 7,
+    subTitle: "Proximo a terminar",
+    icon: (
+      <ProductionQuantityLimitsOutlined color="warning" sx={{ fontSize: 40 }} />
+    ),
+  },
+  {
+    title: 8,
+    subTitle: "Actualizacion en:",
+    icon: <AccessTimeOutlined color="secondary" sx={{ fontSize: 40 }} />,
+  },
+];
 
 const AdminDashboradPage = () => {
   return (
@@ -8,7 +65,16 @@ const AdminDashboradPage = () => {
       subtitle="Estadisticas generales"
       icon={<DashboardOutlined />}
     >
-        <h3>Hola mubdi</h3>
+      <Grid container spacing={2}>
+        {summaryTile.map(({ title, subTitle, icon }) => (
+          <AdminSummary
+            key={title}
+            title={title}
+            subTitle={subTitle}
+            icon={icon}
+          />
+        ))}
+      </Grid>
     </AdminLayout>
   );
 };
