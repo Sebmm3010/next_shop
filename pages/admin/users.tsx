b17d358159dc9dc3b9f059be4a453243883d0124
Sebastian madero <rockmadero@gmail.com>
2023-04-09 11:30:28 -0500

    users table working

--- pages/admin/users.tsx
+++ pages/admin/users.tsx
@@ -1,10 +1,11 @@
 import useSWR from "swr";
 import { PeopleOutline } from "@mui/icons-material";
-import { Grid, Typography } from "@mui/material";
-import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import { Grid, MenuItem, Select, Typography } from "@mui/material";
+import { DataGrid, GridColDef, GridRenderCellParams } from "@mui/x-data-grid";
 import { AdminLayout } from "@/components/layouts";
 import { FullScreenLoading } from "@/components/ui";
 import { IUser } from "@/interfaces";
+import { nextShopApi } from "@/api";
 
 const UsersPage = () => {
   const { data, error } = useSWR<IUser[]>("/api/admin/users");
@@ -17,13 +18,41 @@ const UsersPage = () => {
     return <Typography>Error al cargar la información</Typography>;
   }
 
+  const onRoleUpdated = async (userId: string, newRole: string) => {
+    try {
+      await nextShopApi.put("/admin/users", { userId, role: newRole });
+    } catch (error) {
+      console.log(error);
+      alert("No se pudo actualizar el role del usuario");
+    }
+  };
+
   const column: GridColDef[] = [
     { field: "email", headerName: "Correo", width: 250 },
     { field: "name", headerName: "Nombre", width: 300 },
-    { field: "role", headerName: "Rol", width: 300 },
+    {
+      field: "role",
+      headerName: "Rol",
+      width: 300,
+      renderCell: ({ row }: GridRenderCellParams) => {
+        return (
+          <Select
+            onChange={({ target }) => onRoleUpdated(row.id, target.value)}
+            value={row.role}
+            label="Rol"
+            sx={{ width: "300px" }}
+          >
+            <MenuItem value="admin">Admin</MenuItem>
+            <MenuItem value="client">Client</MenuItem>
+            <MenuItem value="super-user">Super-user</MenuItem>
+            <MenuItem value="CEO">SEO</MenuItem>
+          </Select>
+        );
+      },
+    },
   ];
 
-  const rows = data!.map((user) => ({
+  const row = data!.map((user) => ({
     id: user._id || user.id,
     email: user.email,
     name: user.name,
@@ -38,7 +67,7 @@ const UsersPage = () => {
     >
       <Grid container className="fadeIn">
         <Grid item xs={12} sx={{ height: 650, width: "100%" }}>
-          <DataGrid rows={rows} columns={column} pageSizeOptions={[100]} />
+          <DataGrid rows={row} columns={column} pageSizeOptions={[100]} />
         </Grid>
       </Grid>
     </AdminLayout>
