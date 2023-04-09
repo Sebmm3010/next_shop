36e3be40d0a041dc7663de8857327f9e79a58609
Sebastian madero <rockmadero@gmail.com>
2023-04-09 09:29:48 -0500

    new roles

--- models/User.ts
+++ models/User.ts
@@ -1,23 +1,25 @@
 import { IUser } from "@/interfaces";
 import mongoose, { Schema, model, Model } from "mongoose";
 
-const userSchema = new Schema({
-  name: { type: String, require: true },
-  email: { type: String, require: true, unique: true },
-  password: { type: String, require: true },
-  role: {
-    type: String,
-    enum: {
-      values: ["admin", "client"],
-      message: "{VALUE} no es un role permitido",
-      default: "client",
-      required: true,
+const userSchema = new Schema(
+  {
+    name: { type: String, require: true },
+    email: { type: String, require: true, unique: true },
+    password: { type: String, require: true },
+    role: {
+      type: String,
+      enum: {
+        values: ["admin", "client", "super-user", "CEO"],
+        message: "{VALUE} no es un role permitido",
+        default: "client",
+        required: true,
+      },
     },
   },
-},{
-    timestamps:true,
-
-});
+  {
+    timestamps: true,
+  }
+);
 
 const User:Model<IUser>=mongoose.models.User || model("User", userSchema);
 export default User;
