f19a951f13efa1a54b442a56302540c369c8be8e
Sebastian madero <rockmadero@gmail.com>
2023-04-11 11:55:26 -0500

    metodo put admin products

--- pages/api/admin/products.ts
+++ pages/api/admin/products.ts
@@ -2,8 +2,9 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { IProduct } from "@/interfaces";
 import { db } from "@/data";
 import { Product } from "@/models";
+import { isValidObjectId } from "mongoose";
 
-type Data = { msg: string } | IProduct[];
+type Data = { msg: string } | IProduct[] | IProduct;
 
 export default function handler(
   req: NextApiRequest,
@@ -11,18 +12,55 @@ export default function handler(
 ) {
   switch (req.method) {
     case "GET":
-      return getProducts(req, res);
+      return getProduct(req, res);
     case "PUT":
+      return updateProduct(req, res);
     case "POST":
 
     default:
       return res.status(400).json({ msg: "Bad request" });
   }
 }
-const getProducts = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+const getProduct = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
   await db.connect();
-  const products = await Product.find().sort({ title: "asc" }).lean();
+  const product = await Product.find().sort({ title: "asc" }).lean();
   await db.disconnect();
   //TODO: actualizar imagenes
-  return res.status(200).json(products);
+  return res.status(200).json(product);
+};
+const updateProduct = async (
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) => {
+  const { _id = "", images = [] } = req.body as IProduct;
+
+  if (!isValidObjectId(_id)) {
+    return res.status(400).json({ msg: `El ID: ${_id} no es un id valido` });
+  }
+
+  if (images.length < 2) {
+    return res
+      .status(400)
+      .json({ msg: "El producto debe tener minimo 2 imagenes" });
+  }
+
+  try {
+    await db.connect();
+    const product = await Product.findById(_id);
+    if (!product) {
+      await db.disconnect();
+      return res
+        .status(400)
+        .json({ msg: `No se encontro el producto con el ID:${_id}` });
+    }
+
+    // ?Eliminar fotos de cloudinary
+    await product.updateOne(req.body);
+    await db.disconnect();
+    res.status(200).json(product);
+  } catch (error) {
+    await db.disconnect();
+    console.log(error);
+    return res.status(400).json({ msg: "Revisar la consola del servidor" });
+  }
 };
