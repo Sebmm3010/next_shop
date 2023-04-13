a5feabc123bbba2cc49f77d18484ba10e2052aee
Sebastian madero <rockmadero@gmail.com>
2023-04-13 15:33:20 -0500

    upload img done

--- pages/admin/products/[slug].tsx
+++ pages/admin/products/[slug].tsx
@@ -127,14 +127,19 @@ const ProductAdminPage: FC<Props> = ({ product }) => {
     setValue("tags", updateTags, { shouldValidate: true });
   };
 
-  const onFilesSelected = ({ target }: ChangeEvent<HTMLInputElement>) => {
+  const onFilesSelected = async ({ target }: ChangeEvent<HTMLInputElement>) => {
     if (!target.files || target.files.length === 0) {
       return;
     }
     try {
       for (const file of target.files) {
         const formData = new FormData();
-        console.log(file);
+        formData.append("file", file);
+        const { data } = await nextShopApi.post<{ msg: string }>(
+          "/admin/upload",
+          formData
+        );
+        console.log(data);
       }
     } catch (error) {
       console.log(error);
