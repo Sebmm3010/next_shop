608e617c6cedd10c5222cf52f6be76ebd9de8b6a
Sebastian madero <rockmadero@gmail.com>
2023-04-16 11:19:41 -0500

    updating metatags
    Mejorando los metatags para el seo

--- components/layouts/ShopLayout.tsx
+++ components/layouts/ShopLayout.tsx
@@ -1,4 +1,4 @@
-import Head from "next/head"
+import Head from "next/head";
 import { FC, ReactNode } from "react";
 import { Navbar, SideMenu } from "../ui";
 
@@ -9,40 +9,48 @@ interface Props {
   imageUrl?: string;
 }
 
-export const ShopLayout: FC<Props> = ({ children, title, pageDesc, imageUrl }) => {
+export const ShopLayout: FC<Props> = ({
+  children,
+  title,
+  pageDesc,
+  imageUrl,
+}) => {
   return (
     <>
       <Head>
         <title>{title}</title>
-        <meta name="og:name" content={title} />
+        <meta name="author" content="Sebastian Madero" />
         <meta name="description" content={pageDesc} />
-        <meta name="og:description" content={pageDesc} />
-        {
-          imageUrl &&(
-            <meta name="og:image" content={ imageUrl } />
-          )
-        }
+        <meta name="keywords" content={`${title}, tienda, ropa`} />
+        <meta property="og:title" content={`Informacion sobre ${title}`} />
+        <meta property="og:description" content={pageDesc} />
+        {imageUrl && (
+          <>
+            <meta property="og:image" content={imageUrl} />
+            <meta property="og:type" content="image" />
+          </>
+        )}
       </Head>
 
       <nav>
-        <Navbar/>
+        <Navbar />
       </nav>
 
       {/* Sidebar */}
 
-      <SideMenu/>
+      <SideMenu />
 
-      <main style={{
-        margin: '80px auto',
-        maxWidth: '1440px',
-        padding: '0 30px'
-      }}>
+      <main
+        style={{
+          margin: "80px auto",
+          maxWidth: "1440px",
+          padding: "0 30px",
+        }}
+      >
         {children}
       </main>
 
-      <footer>
-        {/* Todo: */}
-      </footer>
+      <footer>{/* Todo: */}</footer>
     </>
-  )
-}
+  );
+};
